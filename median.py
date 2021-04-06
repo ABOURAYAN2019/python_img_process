4c17d595337af348b853288251e7d0af1d8c6e83
Your Name <you@example.com>
2021-04-06 21:47:19 +0100

    test

--- median.py
+++ median.py
@@ -19,27 +19,23 @@ print(np.min(I2) ,np.max(I2) )
 def median_filter(data, filter_size=5):
     temp = []
     indexer = filter_size // 2
-    data_final = []
-    data_final = np.zeros((len(data),len(data[0])))
+    window = [
+        (i, j)
+        for i in range(-indexer, filter_size-indexer)
+        for j in range(-indexer, filter_size-indexer)
+    ]
+    index = len(window) // 2
     for i in range(len(data)):
-
         for j in range(len(data[0])):
-
-            for z in range(filter_size):
-                if i + z - indexer < 0 or i + z - indexer > len(data) - 1:
-                    for c in range(filter_size):
-                        temp.append(0)
-                else:
-                    if j + z - indexer < 0 or j + indexer > len(data[0]) - 1:
-                        temp.append(0)
-                    else:
-                        for k in range(filter_size):
-                            temp.append(data[i + z - indexer][j + k - indexer])
-
-            temp.sort()
-            data_final[i][j] = temp[len(temp) // 2]
-            temp = []
-    return data_final
+            data[i][j] = sorted(
+                0 if (
+                    min(i+a, j+b) < 0
+                    or len(data) <= i+a
+                    or len(data[0]) <= j+b
+                ) else data[i+a][j+b]
+                for a, b in window
+            )[index]
+    return data
 
 plt.imshow(median_filter(I2) ,'gray')
 plt.show()
